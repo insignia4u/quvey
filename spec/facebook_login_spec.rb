9e65004c77d10397df4018b0106cc38b3a19370d
Manuel Ramos <bymram@gmail.com>
2013-04-15 11:05:02 -0300

    Test facebook_login

--- spec/facebook_login_spec.rb
+++ spec/facebook_login_spec.rb
@@ -1,7 +1,16 @@
 require "spec_helper"
 
 feature "Facebook login" do
+  let!(:oauth_data) { facebook_auth_data }
+  #let!(:invalid_data) { facebook_auth_data (valid = false)}
 
+  #background do
+  #  populate_homepage!
+
+  #  visit root_path
+  #  click_link "Login"
+
+  #end
   scenario "Signing in with a facebook account" do
     OmniAuth.config.mock_auth[:facebook] = oauth_data
     #click_on "Login with facebook"
