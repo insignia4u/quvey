5a4d111c0c5349612ceb765fbddbf54e6307f5c2
Manuel Ramos <bymram@gmail.com>
2013-04-25 09:12:46 -0300

    Fixes

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,17 +1,9 @@
 class Question < ActiveRecord::Base
   belongs_to :survey
   belongs_to :question_type
-  has_many   :question_possible_value
+  has_many   :question_possible_values
 
   validates :survey, presence: true
   validates :question_type, presence: true
   validates :title, presence: true, uniqueness: { scope: :survey_id }
-  validate  :possible_value_not_assign
-  
-protected
-  def possible_value_not_assign
-      unless question_possible_value
-        errors.add(:without_posible_values, "dasdasdsa")
-      end
-  end
 end
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -2,7 +2,6 @@ FactoryGirl.define do
   factory :question do
     survey
     question_type
-    #question_possible_value
 
     sequence(:title) { |n| "Title#{n}" }
   end
