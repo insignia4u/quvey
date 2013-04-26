ad7f2066990ffb23b4722e4ac6cfdb52220261bd
Manuel Ramos <bymram@gmail.com>
2013-04-26 18:25:58 -0300

    Added the validation for question without question_possible_value

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,19 +1,21 @@
 class Question < ActiveRecord::Base
   belongs_to :survey
   belongs_to :question_type
-  has_many   :question_possible_values
 
   validates :survey, presence: true
   validates :question_type, presence: true
   validates :title, presence: true, uniqueness: { scope: :survey_id }
   validate  :validate_question
-  
+
   protected
   def validate_question
-    if (question_type == 'multiple-choise') || (question_type == 'options')
-      unless question_posible_value
-        errors.add(:base, "question requires question_posible_value presence")
+    if question_type
+      if (question_type.name == 'multiple-choise') || (question_type.name == 'options')
+        unless question_posible_value
+          errors.add(:base, "question requires question_posible_value presence")
+        end
       end
     end
   end
 end
+
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,26 +1,42 @@
 require 'spec_helper'
 
 describe Question do
+
+  describe "Relations" do
+    it { should belong_to(:survey) }
+    it { should belong_to(:question_type) }
+  end
+
   describe "Validations" do
-    it { should validate_presence_of(:title) }
     it { should validate_presence_of(:survey) }
     it { should validate_presence_of(:question_type) }
+    it { should validate_presence_of(:title) }
     it { should validate_uniqueness_of(:title).scoped_to(:survey_id) }
 
     it "should requires question invalid" do
       expect(subject).to_not be_valid
     end
   end
-  
-  describe "should requires question_possible_value presence" do
+
+  describe "should require the presence of question_possible_value" do
     context "With option multiple-choise" do
       before(:each) do
-        question = FactoryGirl.build(:question).question_type.name = "multiple-choise" 
+        @question = FactoryGirl.create(:question).question_type.name = 'multiple-choise'
       end
-      
+
       it "Should be a invalid question" do
-        expect(:question).to have_at_least(1).errors_on(:base)
+        expect(@question).to have_at_least(1).errors_on(:base)
+     end
+    end
+
+    context "With option options" do
+      before(:each) do
+        @question = FactoryGirl.create(:question).question_type.name = 'options'
       end
+
+      it "Should be a invalid question" do
+        expect(@question).to have_at_least(1).errors_on(:base)
+     end
     end
   end
 
