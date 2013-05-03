42a67323cc93884de0c920239c0e491d0cb6c6ab
Manuel Ramos <bymram@gmail.com>
2013-05-03 10:47:40 -0300

    Added spec create_survey and created view for new surveys

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Quvey::Application.routes.draw do
   match 'auth/:provider/callback', to: 'sessions#create'
   match 'auth/failure', to: redirect('/')
   match 'signout',      to: 'sessions#destroy', as: 'signout'
+  match '/survey/new', to: 'surveys#new', as: 'new_survey'
+
+  resources :surveys, only: [:new, :create]
 
   get "home/index"
   root :to => 'home#index'
