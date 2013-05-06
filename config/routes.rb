1ff1620d039b9cb8ed5f1ec199f4297be3ac85ee
Manuel Ramos <bymram@gmail.com>
2013-05-06 09:26:50 -0300

    Added settings for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Quvey::Application.routes.draw do
   match 'auth/:provider/callback', to: 'sessions#create'
   match 'auth/failure', to: redirect('/')
   match 'signout',      to: 'sessions#destroy', as: 'signout'
-  match '/survey/new', to: 'surveys#new', as: 'new_survey'
+  match '/surveys/new', to: 'surveys#new', as: 'new_survey'
 
   resources :surveys, only: [:new, :create]
 
