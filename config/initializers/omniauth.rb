30544dccb03bcc6059f36d636645d4b319849516
Manuel Ramos <bymram@gmail.com>
2013-05-07 12:42:00 -0300

    Setting figaro gem

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '136360799881017', '9925bb5126d7f60445c8f026797a9210'
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
 end
-
